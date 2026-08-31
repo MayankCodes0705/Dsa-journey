a03e99f0415805abf2288f32206507b6d69c6a62
Mayank Goyal <goyalmayank1007@gmail.com>
2026-08-31 22:17:56 +0530

    LeetCode: update cpp/running_sum_of_1d_array.cpp

--- cpp/running_sum_of_1d_array.cpp
+++ cpp/running_sum_of_1d_array.cpp
@@ -3,16 +3,21 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/running-sum-of-1d-array/
 // Synced by: LinkCode
-// Date: 8/31/2026, 10:09:16 PM
+// Date: 8/31/2026, 10:17:53 PM
 // ======================================
 
 
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans(nums.size());
+
+        ans[0] = nums[0];
+
         for (int i = 1; i < nums.size(); i++) {
-            nums[i] += nums[i - 1];
+            ans[i] = ans[i - 1] + nums[i];
         }
-        return nums;
+
+        return ans;
     }
 };
