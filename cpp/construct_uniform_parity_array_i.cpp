71986857b2308b02c557d7cefb3042e4025f732d
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-02 22:59:07 +0530

    LeetCode: update cpp/construct_uniform_parity_array_i.cpp

---
+++ cpp/construct_uniform_parity_array_i.cpp
@@ -0,0 +1,16 @@
+// ======================================
+// LeetCode Problem: construct uniform parity array i
+// Language: cpp
+// Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+// Synced by: LinkCode
+// Date: 9/2/2026, 10:59:07 PM
+// ======================================
+
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+        
+    }
+};
