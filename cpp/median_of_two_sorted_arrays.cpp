759311f00c1b7f5e1e20ee4a8f4e985e7a753c94
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-03 23:32:31 +0530

    LeetCode: update cpp/median_of_two_sorted_arrays.cpp

---
+++ cpp/median_of_two_sorted_arrays.cpp
@@ -0,0 +1,16 @@
+// ======================================
+// LeetCode Problem: median of two sorted arrays
+// Language: cpp
+// Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+// Synced by: LinkCode
+// Date: 9/3/2026, 11:32:31 PM
+// ======================================
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.length();
+        int n = nums2.length();
+    }
+};
