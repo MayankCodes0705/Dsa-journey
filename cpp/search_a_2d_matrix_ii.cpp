3ccd9c219bd65d7d2fc2e89f0ddbfcb3a4151cbd
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-05 02:29:05 +0530

    LeetCode: update cpp/search_a_2d_matrix_ii.cpp

--- cpp/search_a_2d_matrix_ii.cpp
+++ cpp/search_a_2d_matrix_ii.cpp
@@ -3,38 +3,29 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/search-a-2d-matrix-ii/
 // Synced by: LinkCode
-// Date: 9/5/2026, 1:47:12 AM
+// Date: 9/5/2026, 2:29:03 AM
 // ======================================
 
 
 class Solution {
 public:
-    bool binarySearch(vector<int>& arr, int target) {
-        int low = 0;
-        int high = arr.size() - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (arr[mid] == target)
-                return true;
-
-            else if (arr[mid] < target)
-                low = mid + 1;
-
-            else
-                high = mid - 1;
-        }
-
-        return false;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
+        int m = matrix[0].size();
 
-        for (int i = 0; i < n; i++) {
-            if (binarySearch(matrix[i], target))
+        int row = n - 1;
+        int col = 0;
+
+        while (row >= 0 && col < m) {
+            if (matrix[row][col] == target) {
                 return true;
+            }
+            else if (matrix[row][col] < target) {
+                col++;
+            }
+            else {
+                row--;
+            }
         }
 
         return false;
