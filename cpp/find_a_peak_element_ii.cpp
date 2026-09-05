643e287938530f1fcc6a35ef4be7ff12958ef45b
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-06 00:04:28 +0530

    LeetCode: update cpp/find_a_peak_element_ii.cpp

--- cpp/find_a_peak_element_ii.cpp
+++ cpp/find_a_peak_element_ii.cpp
@@ -3,33 +3,58 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/find-a-peak-element-ii/
 // Synced by: LinkCode
-// Date: 9/5/2026, 10:42:02 PM
+// Date: 9/6/2026, 12:04:26 AM
 // ======================================
 
 
 class Solution {
 public:
+
+    int findMaxIndex(vector<vector<int>>& mat, int n, int m, int col) {
+        int maxValue = -1;
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (mat[i][col] > maxValue) {
+                maxValue = mat[i][col];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
 
-        int m = mat.size();
-        int n = mat[0].size();
+        int low = 0;
+        int high = m - 1;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-                if (i > 0 && mat[i][j] <= mat[i-1][j])
-                    continue;
+            int maxRowIndex = findMaxIndex(mat, n, m, mid);
 
-                if (i < m-1 && mat[i][j] <= mat[i+1][j])
-                    continue;
+            int current = mat[maxRowIndex][mid];
 
-                if (j > 0 && mat[i][j] <= mat[i][j-1])
-                    continue;
+            int left = -1;
+            int right = -1;
 
-                if (j < n-1 && mat[i][j] <= mat[i][j+1])
-                    continue;
+            if (mid - 1 >= 0)
+                left = mat[maxRowIndex][mid - 1];
 
-                return {i, j};
+            if (mid + 1 < m)
+                right = mat[maxRowIndex][mid + 1];
+
+            if (current > left && current > right) {
+                return {maxRowIndex, mid};
+            }
+            else if (current < left) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
             }
         }
 
