d927d3727632aa9e778e9ad32a5811a39634db8a
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-07 00:02:40 +0530

    LeetCode: update cpp/remove_outermost_parentheses.cpp

---
+++ cpp/remove_outermost_parentheses.cpp
@@ -0,0 +1,31 @@
+// ======================================
+// LeetCode Problem: remove outermost parentheses
+// Language: cpp
+// Link: https://leetcode.com/problems/remove-outermost-parentheses/
+// Synced by: LinkCode
+// Date: 9/7/2026, 12:02:39 AM
+// ======================================
+
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int count = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                if (count > 0)
+                    ans += ch;
+                count++;
+            }
+            else {
+                count--;
+                if (count > 0)
+                    ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};
