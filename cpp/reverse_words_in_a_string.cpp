bd2fb7044fe8cec0f033f318f2841423ec32c7a9
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-08 00:09:11 +0530

    LeetCode: update cpp/reverse_words_in_a_string.cpp

---
+++ cpp/reverse_words_in_a_string.cpp
@@ -0,0 +1,31 @@
+// ======================================
+// LeetCode Problem: reverse words in a string
+// Language: cpp
+// Link: https://leetcode.com/problems/reverse-words-in-a-string/
+// Synced by: LinkCode
+// Date: 9/8/2026, 12:09:10 AM
+// ======================================
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        vector<string> words;
+        string word;
+
+        while (ss >> word) {
+            words.push_back(word);
+        }
+
+        reverse(words.begin(), words.end());
+
+        string ans;
+        for (int i = 0; i < words.size(); i++) {
+            if (i > 0) ans += " ";
+            ans += words[i];
+        }
+
+        return ans;
+    }
+};
