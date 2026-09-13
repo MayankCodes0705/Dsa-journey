860e3261bec413324b486f8c8f2fdfbd80dc7769
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-14 00:03:20 +0530

    LeetCode: update cpp/longest_common_prefix.cpp

--- cpp/longest_common_prefix.cpp
+++ cpp/longest_common_prefix.cpp
@@ -3,24 +3,22 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/longest-common-prefix/
 // Synced by: LinkCode
-// Date: 9/13/2026, 10:40:30 PM
+// Date: 9/14/2026, 12:03:19 AM
 // ======================================
 
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
+        string ans = strs[0];
 
-        for(int i = 0; i < strs[0].size(); i++) {
-            char ch = strs[0][i];
+        for(int i = 1; i < strs.size(); i++) {
+            int j = 0;
 
-            for(int j = 1; j < strs.size(); j++) {
-                if(i >= strs[j].size() || strs[j][i] != ch)
-                    return ans;
-            }
+            while(j < ans.size() && j < strs[i].size() && ans[j] == strs[i][j])
+                j++;
 
-            ans += ch;
+            ans = ans.substr(0, j);
         }
 
         return ans;
