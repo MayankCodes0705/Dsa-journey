e9fc4ae8a5d0c7d9318dc895b719d9898b91000a
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-16 01:28:23 +0530

    LeetCode: update cpp/rotate_string.cpp

--- cpp/rotate_string.cpp
+++ cpp/rotate_string.cpp
@@ -3,25 +3,18 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/rotate-string/
 // Synced by: LinkCode
-// Date: 9/16/2026, 1:06:38 AM
+// Date: 9/16/2026, 1:28:22 AM
 // ======================================
 
 
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n = s.length();
-        int m = goal.length();
-        if(n!=m) return false;
-        for(int i =0; i<n;i++){
-            if(s==goal){
-                return true;
-            }
-            char ch = s[0];
-            s.erase(0,1);
-            s += ch;
-        }
+        if(s.length() != goal.length())
+            return false;
 
-        return false;
+        string temp = s + s;
+
+        return temp.find(goal) != string::npos;
     }
 };
