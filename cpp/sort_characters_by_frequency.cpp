57d4317a0338746ef8e689b7f1c459cbd1e9df10
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-17 00:45:51 +0530

    LeetCode: update cpp/sort_characters_by_frequency.cpp

---
+++ cpp/sort_characters_by_frequency.cpp
@@ -0,0 +1,30 @@
+// ======================================
+// LeetCode Problem: sort characters by frequency
+// Language: cpp
+// Link: https://leetcode.com/problems/sort-characters-by-frequency/
+// Synced by: LinkCode
+// Date: 9/17/2026, 12:45:50 AM
+// ======================================
+
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>freq;
+        for(char ch: s){
+            freq[ch]++;
+        }
+
+        vector<pair<char,int>>v(freq.begin(),freq.end());
+
+        sort(v.begin(),v.end(),[](auto &a,auto &b){
+            return a.second > b.second;
+        });
+
+        string ans = "";
+        for(auto p: v){
+            ans += string(p.second,p.first);
+        }
+        return ans;
+    }
+};
