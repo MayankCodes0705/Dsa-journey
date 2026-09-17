dd9a54f662a50ad5f2eca17f1d407467a0d9fc8f
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-17 23:10:50 +0530

    LeetCode: update cpp/string_to_integer_atoi.cpp

---
+++ cpp/string_to_integer_atoi.cpp
@@ -0,0 +1,53 @@
+// ======================================
+// LeetCode Problem: string to integer atoi
+// Language: cpp
+// Link: https://leetcode.com/problems/string-to-integer-atoi/
+// Synced by: LinkCode
+// Date: 9/17/2026, 11:10:49 PM
+// ======================================
+
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.length();
+
+        while(i<n && s[i]==' '){
+            i++;
+        }
+
+        int sign = 1;
+        if(i<n && s[i]=='-'){
+            sign = -1;
+            i++;
+        }else if(i<n && s[i]=='+'){
+            i++;
+        }
+
+        string num = "";
+        while(i < n && isdigit(s[i])){
+            num += s[i];
+            i++;
+        }
+
+        long long ans = 0;
+        int j = 0;
+
+        while(j < num.length()){
+            ans = ans * 10 + (num[j]-'0');
+            if(ans * sign > INT_MAX){
+                return INT_MAX;
+            }
+            if(ans * sign < INT_MIN){
+                return INT_MIN;
+            }
+            j++;
+        }
+        
+
+        
+     return ans * sign; 
+
+    }
+};
