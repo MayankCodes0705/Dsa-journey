0d64c3a5faf1840582a91486e1f318eeab5dde94
Mayank Goyal <goyalmayank1007@gmail.com>
2026-09-26 01:38:19 +0530

    LeetCode: update cpp/intersection_of_two_linked_lists.cpp

--- cpp/intersection_of_two_linked_lists.cpp
+++ cpp/intersection_of_two_linked_lists.cpp
@@ -3,7 +3,7 @@
 // Language: cpp
 // Link: https://leetcode.com/problems/intersection-of-two-linked-lists/
 // Synced by: LinkCode
-// Date: 9/26/2026, 12:55:19 AM
+// Date: 9/26/2026, 1:38:19 AM
 // ======================================
 
 
@@ -17,19 +17,24 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp = headA;
-        map<ListNode*,int>mp;
-        while(temp!=NULL){
-            mp[temp] = 1;
-            temp = temp->next;
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
+
+        while(t1 != t2) {
+
+            if(t1 == NULL)
+                t1 = headB;
+            else
+                t1 = t1->next;
+
+            if(t2 == NULL)
+                t2 = headA;
+            else
+                t2 = t2->next;
         }
 
-        ListNode* temp1 = headB;
-        while(temp1 != NULL){
-            if(mp.find(temp1)!= mp.end()) return temp1;
-            temp1=temp1->next;
-        }
-        return NULL;
+        return t1;
     }
 };
